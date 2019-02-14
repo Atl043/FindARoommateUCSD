554e860e055fcf69249941eb18daab35383fc787
atl043 <atl043@ucsd.edu>
2019-02-13 18:02:36 -0800

    facebook test 1

--- app.js
+++ app.js
@@ -18,6 +18,25 @@ var createProfile = require('./routes/createProfile');
 // Example route
 // var user = require('./routes/user');
 
+// facebook start
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+  clientID: "352041492055393",
+  clientSecret: "e4a74fb20b4494a279ddac434888257d",
+  callbackURL: "https://find-a-roommate-ucsd.herokuapp.com/"
+},
+  function (accessToken, refreshToken, profile, done) {
+    User.findOrCreate(function (err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+// end of facebook
+
+
 var app = express();
 
 // all environments
@@ -46,10 +65,29 @@ app.get('/profile', profile.viewProfile);
 app.get('/login', login.viewLogin);
 app.get('/user', user.view_userProfile);
 app.get('/settings', settings.viewSettings);
-
 app.get('/createProfile', createProfile.viewCreateProfile);
 // Example route
 // app.get('/users', user.list);
+
+// facebook start
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/createProfile',
+    failureRedirect: '/login'
+  }));
+// app.get('/auth/facebook',
+//   passport.authenticate('facebook', { scope: ['read_stream', 'publish_actions'] })
+// );
+// facebook end
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
