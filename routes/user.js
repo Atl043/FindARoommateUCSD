45d85e9d415291ec8ed3e766acad48c5b920136a
zainbaq <sbaquar@ucsd.edu>
2019-02-14 21:14:59 -0800

    updated

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,7 @@ exports.view_userProfile = function (req, res) {
             'email': req.query.email,
             'imgurl': req.query.imgurl,
             'address': req.query.address,
+            'userstate': 'Offering',
             'zip': req.query.zip,
             'roomsize': req.query.roomsize,
             'gender': req.query.gender,
@@ -30,6 +31,37 @@ exports.view_userProfile = function (req, res) {
             'otherroommateNotes': req.query.otherroommateNotes
         };
         console.log(json);
+        data.user.push(json);
+    } else if (req.query.userstate == 1) {
+        json = {
+            'name': req.query.name,
+            'email': req.query.email,
+            'imgurl': req.query.imgurl,
+            'address': req.query.address,
+            'userstate': 'Looking',
+            'zip': req.query.zip,
+            'roomsize': req.query.roomsize,
+            'gender': req.query.gender,
+            'min-rent': req.query.min-rent,
+            'max-rent': req.query.max-rent,
+            'rent': req.query.min-rent + '-' + req.query.max-rent,
+            'ownBath': req.query.ownBath,
+            'AC': req.query.AC,
+            'heating': req.query.heating,
+            'laundry': req.query.laundry,
+            'parking': req.query.parking,
+            'pets': req.query.pets,
+            'bus': req.query.bus,
+            'gym': req.query.gym,
+            'pool': req.query.pool,
+            'otherapartmentNotes': req.query.otherapartmentNotes,
+            'timeofday': req.query.timeofday,
+            'smokersetting': req.query.smokersetting,
+            'cleanliness': req.query.cleanliness,
+            'otherroommateNotes': req.query.otherroommateNotes
+        };
+        console.log('sejoifcs');
+        console.log(json)
         data.user.push(json);
     }
     console.log("teste")
