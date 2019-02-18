5b6eff8aeb9aa7fabcf4e13f77819e0bab13faca
atl043 <atl043@ucsd.edu>
2019-02-18 14:54:47 -0800

    images now work for profiles

--- routes/profile.js
+++ routes/profile.js
@@ -5,9 +5,9 @@ var images = require('../pictureData.json');
 var data = require('../profileData.json');
 exports.viewProfile = function (req, res) {
     var id = req.params.id;
-    console.log("The id is: " + id);
-    console.log("images test" + JSON.stringify(images[id][0]));
-    console.log("data" + JSON.stringify(data[id][0].name));
+    // console.log("The id is: " + id);
+    // console.log("images test" + JSON.stringify(images[id][0]));
+    // console.log("data" + JSON.stringify(data[id][0].name));
     res.render('profile', {
         'id': id,
         'images': images[id],
