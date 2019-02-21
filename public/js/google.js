d3a73f5c730de611d6f4934286141e5ddf86a680
atl043 <atl043@ucsd.edu>
2019-02-20 18:00:55 -0800

    got log out to work for setting page for google

--- public/js/google.js
+++ public/js/google.js
@@ -45,3 +45,10 @@ function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
 }
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
