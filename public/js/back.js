81d6e48ab4f54221827e4db48c4a360e68c53ae8
Andrew T. Li <andrewfourli@gmail.com>
2019-03-06 19:50:18 -0800

    Bug fix from A8
    1) User profile does not show long descriptions
    2) “Is ‘ok with smoke’ for smoker tolerance?”
    3) Difficulty finding the sign-out

---
+++ public/js/back.js
@@ -0,0 +1,17 @@
+function goBack() {
+    window.history.back();
+}
+
+function signOutTest() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    window.location.href = "/";
+}
+onLoad()
+function onLoad() {
+    gapi.load('auth2', function () {
+        gapi.auth2.init();
+    });
+}
