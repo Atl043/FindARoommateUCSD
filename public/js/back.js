5d69a5402bbc205868b60537936f8a9402f80fc6
atl043 <andrewfourli@gmail.com>
2019-03-14 13:11:07 -0700

    test 6

--- public/js/back.js
+++ public/js/back.js
@@ -5,13 +5,13 @@ function goBack() {
 function signOutTest() {
     var auth2 = gapi.auth2.getAuthInstance();
     FB.init({
-        appId: '352041492055393',
+        appId: 352041492055393,
         cookie: true,  // enable cookies to allow the server to access 
         // the session
         xfbml: true,  // parse social plugins on this page
         version: 'v3.2' // The Graph API version to use for the call
     });
-    FB.getLoginStatus();
+    FB.getLoginStatus(handleSessionResponse);
     FB.logout(function (response) {
         console.log('User signed out FB.');// user is now logged out
     });
@@ -20,6 +20,19 @@ function signOutTest() {
     });
     window.location.href = "/";
 }
+//handle a session response from any of the auth related calls
+function handleSessionResponse(response) {
+    //if we dont have a session (which means the user has been logged out, redirect the user)
+    if (!response.session) {
+        window.location = "/mysite/Login.aspx";
+        return;
+    }
+
+    //if we do have a non-null response.session, call FB.logout(),
+    //the JS method will log the user out of Facebook and remove any authorization cookies
+    FB.logout(handleSessionResponse);
+}
+
 function onLoad() {
     gapi.load('auth2', function () {
         gapi.auth2.init();
