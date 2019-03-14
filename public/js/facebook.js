c5e9a7269e09a0f14115ec2d370dc7a3f708a91e
atl043 <andrewfourli@gmail.com>
2019-03-14 11:54:12 -0700

    facebook test 2

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -20,16 +20,6 @@ function statusChangeCallback(response) {
 
 
 }
-FB.login(function (response) {
-  if (response.authResponse) {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function (response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  } else {
-    console.log('User cancelled login or did not fully authorize.');
-  }
-});
 
 function changeUser(response) {
   $('.facebookLogin').hide();
