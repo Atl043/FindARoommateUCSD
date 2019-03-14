08febd52898c0eafd5d84a73a66d1b7eb17d9460
atl043 <andrewfourli@gmail.com>
2019-03-14 12:59:47 -0700

    test 5

--- public/js/back.js
+++ public/js/back.js
@@ -4,6 +4,14 @@ function goBack() {
 
 function signOutTest() {
     var auth2 = gapi.auth2.getAuthInstance();
+    FB.init({
+        appId: '352041492055393',
+        cookie: true,  // enable cookies to allow the server to access 
+        // the session
+        xfbml: true,  // parse social plugins on this page
+        version: 'v3.2' // The Graph API version to use for the call
+    });
+    FB.getLoginStatus(handleSessionResponse);
     FB.logout(function (response) {
         console.log('User signed out FB.');// user is now logged out
     });
